a1b7f73bbd8de4cc940b71823ce3a25ae0057c9d
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-04-10 03:55:32 +0800

    init

---
+++ src/test/java/me/yogendra/apidemo/ApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yogendra.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
